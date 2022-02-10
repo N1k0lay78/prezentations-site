0eb22ee26e27a0988df52e5591c15aa9895d24b5
Rjkzavr <kolya.toropof@gmail.com>
2022-02-10 22:34:10 +0300

    feature change slide on Wheel and Arrow Up/Down
    fix change slides on mouse wheel and arrow up/down

--- static/js/slides.js
+++ static/js/slides.js
@@ -1,6 +1,8 @@
 let current_slide = 1;
+let isOnPresentationMode = true;
 let slides = document.getElementsByClassName("slide");
 let last_slide = slides.length;
+let deltaScroll = 0;
 for (var i = 0; i < slides.length; i++) {
     slides.item(i).setAttribute('data-page', (i+1));
 }
@@ -16,25 +18,45 @@ function set_slide() {
 }
 
 function change_mode() {
+    isOnPresentationMode = !isOnPresentationMode;
     document.getElementById("change-mode").classList.toggle("pressed");
     document.body.classList.toggle("presentation");
     current_slide = Math.round(window.pageYOffset / window.innerHeight) + 1;
     set_slide();
 }
 
-function checkKey(e) {
-    if (event.keyCode === 39 && current_slide < last_slide) {
+function onKey(e) {
+    if ((event.keyCode === 39 || event.keyCode === 40) && current_slide < last_slide) {
         current_slide += 1;
-    } else if (event.keyCode === 37 && current_slide > 1) {
+    } else if ((event.keyCode === 37 || event.keyCode === 38) && current_slide > 1) {
         current_slide -= 1;
-        let slides = document.getElementsByClassName("slide");
     }
     set_slide();
 }
 
-function checkScroll(e) {
+function onScroll(e) {
     document.getElementById("current-slide").innerText = Math.round(window.pageYOffset / window.innerHeight) + 1;
 }
 
-document.onkeyup = checkKey;
-document.onscroll = checkScroll;
+function onWheel(e) {
+    if (isOnPresentationMode) {
+        if (event.deltaY > 0 && deltaScroll < 0) {
+            deltaScroll = 0;
+        } else if (event.deltaY < 0 && deltaScroll > 0) {
+            deltaScroll = 0;
+        }
+        deltaScroll += event.deltaY * 0.01;
+        if (deltaScroll > 3) {
+            deltaScroll = 0;
+            current_slide += 1;
+        } else if (deltaScroll < -3) {
+            deltaScroll = 0;
+            current_slide -= 1;
+        }
+        set_slide();
+    }
+}
+
+document.onkeyup = onKey;
+document.onscroll = onScroll;
+document.onwheel = onWheel;
